b716ede81055d9b02410feea0288ff817ab309c7
Shashanka <poudelshashanka205@gmail.com>
2025-07-22 07:32:21 +0545

    latest changes

--- src/config/api.jsx
+++ src/config/api.jsx
@@ -8,6 +8,6 @@
 const BASE_URL =
     import.meta.env.MODE === "development"
         ? "http://localhost:8000/api"
-        : "https://blogapp-backend-texas.onrender.com/";
+        : "https://blogapp-backend-texas.onrender.com";
 
 export default BASE_URL;
